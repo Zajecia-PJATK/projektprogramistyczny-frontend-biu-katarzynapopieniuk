e829538bc2b90732b45f72217032fec018d84536
Katarzyna Popieniuk <katarzynapopieniuk@gmail.com>
2023-05-20 13:01:39 +0200

    Add basic routes and empty pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import React from "react";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
+            </ul>
+        </nav>
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/cart" element={<CartPage/>}/>
+            <Route path="/products/:id" element={<ProductPage/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
     </div>
   );
 }
